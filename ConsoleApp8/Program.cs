32298db9b2aa6398fb8e1b4bbbd86f351d4bffd4
mrbuzb <mrbpubg14@gmail.com>
2025-09-14 22:28:40 +0500

    1160_completed

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -5,89 +5,115 @@
         static void Main(string[] args)
         {
             var sss = new Solution();
-            Console.WriteLine(sss.FreqAlphabets("12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#"));
+            Console.WriteLine(sss.CountCharacters(["cat", "bt", "hat", "tree"], "atach"));
         }
     }
-
-
 
 
     public class Solution
     {
-        //1309
-        public string FreqAlphabets(string s)
+        //1160
+        public int CountCharacters(string[] words, string chars)
         {
-            var dict = new Dictionary<int, char>()
-        {
-            { 1,'a'},
-            { 2,'b'},
-            { 3,'c'},
-            { 4,'d'},
-            { 5,'e'},
-            { 6,'f'},
-            { 7,'g'},
-            { 8,'h'},
-            { 9,'i'},
-            { 10,'j'},
-            { 11,'k'},
-            { 12,'l'},
-            { 13,'m'},
-            { 14,'n'},
-            { 15,'o'},
-            { 16,'p'},
-            { 17,'q'},
-            { 18,'r'},
-            { 19,'s'},
-            { 20,'t'},
-            { 21,'u'},
-            { 22,'v'},
-            { 23,'w'},
-            { 24,'x'},
-            { 25,'y'},
-            { 26,'z'},
-        };
-            var helper = string.Empty;
-            var res = string.Empty;
-            var counter = 0;
-            if(s.EndsWith('#')) counter = s.Length;
-            else counter = s.Length-1;
-                for (int i = 0; i < s.Length; i++)
+            var result = 0;
+            var checker = true;
+            foreach(var word in words)
+            {
+                foreach(var ch in word)
                 {
-                    if (helper.Length == 2 && s[i] != '#' && s[i + 1] == '#')
+                    if(chars.Count(x=>x == ch) >= word.Count(x=>x == ch))
                     {
-                        res += dict[helper[0] - '0'];
-                        helper = string.Empty;
-                        i--;
-                    }
-                    if (helper.Length == 2 && s[i] != '#' && s[i] != '0')
-                    {
-                        res += dict[helper[0] - '0'];
-                        res += dict[helper[1] - '0'];
-                        helper = string.Empty;
-                        i--;
-                    }
-                    else if (s[i] == '#')
-                    {
-                        res += dict[int.Parse(helper)];
-                        helper = string.Empty;
+                        continue;
                     }
                     else
                     {
-                        helper += s[i];
+                        checker = false;
+                        break;
                     }
                 }
-            if (helper.Length == 2)
-            {
-                res += dict[helper[0] - '0'];
-                res += dict[helper[1] - '0'];
+                if (checker) result += word.Length;
+                checker = true;
             }
-            else if (helper.Length == 1)
-            {
-                res += dict[helper[0] - '0'];
-            }
-            return res;
+            return result;
         }
     }
+
+    //public class Solution
+    //{
+    //    //1309
+    //    public string FreqAlphabets(string s)
+    //    {
+    //        var dict = new Dictionary<int, char>()
+    //    {
+    //        { 1,'a'},
+    //        { 2,'b'},
+    //        { 3,'c'},
+    //        { 4,'d'},
+    //        { 5,'e'},
+    //        { 6,'f'},
+    //        { 7,'g'},
+    //        { 8,'h'},
+    //        { 9,'i'},
+    //        { 10,'j'},
+    //        { 11,'k'},
+    //        { 12,'l'},
+    //        { 13,'m'},
+    //        { 14,'n'},
+    //        { 15,'o'},
+    //        { 16,'p'},
+    //        { 17,'q'},
+    //        { 18,'r'},
+    //        { 19,'s'},
+    //        { 20,'t'},
+    //        { 21,'u'},
+    //        { 22,'v'},
+    //        { 23,'w'},
+    //        { 24,'x'},
+    //        { 25,'y'},
+    //        { 26,'z'},
+    //    };
+    //        var helper = string.Empty;
+    //        var res = string.Empty;
+    //        var counter = 0;
+    //        if(s.EndsWith('#')) counter = s.Length;
+    //        else counter = s.Length-1;
+    //            for (int i = 0; i < s.Length; i++)
+    //            {
+    //                if (helper.Length == 2 && s[i] != '#' && s[i + 1] == '#')
+    //                {
+    //                    res += dict[helper[0] - '0'];
+    //                    helper = string.Empty;
+    //                    i--;
+    //                }
+    //                if (helper.Length == 2 && s[i] != '#' && s[i] != '0')
+    //                {
+    //                    res += dict[helper[0] - '0'];
+    //                    res += dict[helper[1] - '0'];
+    //                    helper = string.Empty;
+    //                    i--;
+    //                }
+    //                else if (s[i] == '#')
+    //                {
+    //                    res += dict[int.Parse(helper)];
+    //                    helper = string.Empty;
+    //                }
+    //                else
+    //                {
+    //                    helper += s[i];
+    //                }
+    //            }
+    //        if (helper.Length == 2)
+    //        {
+    //            res += dict[helper[0] - '0'];
+    //            res += dict[helper[1] - '0'];
+    //        }
+    //        else if (helper.Length == 1)
+    //        {
+    //            res += dict[helper[0] - '0'];
+    //        }
+    //        return res;
+    //    }
+    //}
 
 
 
